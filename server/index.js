45a85ba441f8c72fd374b797bbcc94b1185e450e
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-01-24 20:17:17 -0500

    setup the server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// const { resolve } = require('path');
+// app.use(express.static(resolve(__dirname, '..', 'public')));
+// app.get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`The server is listening on port ${port}`);
+});
