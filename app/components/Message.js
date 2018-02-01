33992caefb62a49f7d842f08f0d80cd2e1355f18
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-01-31 19:26:00 -0500

    styled the message component

--- app/components/Message.js
+++ app/components/Message.js
@@ -9,7 +9,10 @@ export default function Message(props) {
         items.map(item => (
           <li className="message" key={item.id}>
             <img src={require('../../public/avatar.png')} className="avatar" />
-            <p className="message-body"> {item.text}</p>
+            <div className="messageContent">
+              <h5 className="posted-time">2:45 PM</h5>
+              <p className="message-body">{item.text}</p>
+            </div>
           </li>
         ))
       }
